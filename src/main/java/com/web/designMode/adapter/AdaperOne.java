f26364f3d57522168abc1cbf8ffc60d0e29a39e5
jrqushiwen <jrqushiwen@jd.com>
2017-10-10 22:54:18 +0800

    设计模式练习模块

---
+++ src/main/java/com/web/designMode/adapter/AdaperOne.java
@@ -0,0 +1,12 @@
+package com.web.designMode.adapter;
+
+/**
+ * Created by jrqushiwen on 2017-10-10.
+ */
+public class AdaperOne extends RiseConcrete implements Target{
+
+    @Override
+    public void request() {
+        riseRequest();
+    }
+}
---
+++ src/main/java/com/web/designMode/adapter/Target.java
@@ -0,0 +1,10 @@
+package com.web.designMode.adapter;
+
+/**
+ * Created by jrqushiwen on 2017-10-10.
+ */
+public interface Target {
+
+    void request();
+
+}
