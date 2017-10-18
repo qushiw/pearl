3b8d1583975e6fe6cb8fe454bcdd6a2c51e70aa0
jrqushiwen <jrqushiwen@jd.com>
2017-10-18 22:29:34 +0800

    设计模式练习模块

--- src/main/java/com/web/zookeeper/ZookeeperClientHold.java
+++ src/main/java/com/web/zookeeper/ZookeeperClientHold.java
@@ -1,6 +1,9 @@
 package com.web.zookeeper;
 
 import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.apache.zookeeper.CreateMode;
 
 /**
  * @author qushiwen on 2017-09-19.
@@ -12,7 +15,24 @@ public class ZookeeperClientHold {
     private static CuratorFramework curatorFramework;
 
 
+    public static void main(String[] args) {
 
+
+        curatorFramework = CuratorFrameworkFactory.builder().connectString("127.0.0.1:2181")
+                .sessionTimeoutMs(3000)
+                .retryPolicy(new ExponentialBackoffRetry(1000, 3))
+                .build();
+
+
+        try {
+            curatorFramework.create().withMode(CreateMode.EPHEMERAL).forPath("/qsfs/qwe/qwe");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+
+    }
 
 
 
---
+++ src/test/java/com/thread/CommonApache.java
@@ -0,0 +1,37 @@
+package com.thread;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections.Predicate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by jrqushiwen on 2017-10-13.
+ */
+public class CommonApache {
+
+    public static void main(String[] args) {
+
+        List list = new ArrayList();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        CollectionUtils.filter(list, new Predicate(){
+            @Override
+            public boolean evaluate(Object o) {
+                if (o instanceof Integer) {
+                    if (o == 2) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        });
+
+        System.out.println(list);
+    }
+
+
+}
