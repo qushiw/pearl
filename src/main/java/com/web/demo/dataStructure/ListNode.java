d85237f0ab5a3923437b8e243182b2d6a3d8228e
jrqushiwen <jrqushiwen@jd.com>
2020-04-05 09:17:32 +0800

    add dataStructure

---
+++ src/main/java/com/web/demo/dataStructure/ListNode.java
@@ -0,0 +1,19 @@
+package com.web.demo.dataStructure;
+
+/**
+ * @author jrqushiwen
+ * @date 2020/3/30 9:46
+ */
+public class ListNode {
+
+    private int value;
+
+    public ListNode next;
+
+    public ListNode pre;
+
+    public ListNode(int value){
+        this.value = value;
+    }
+
+}
